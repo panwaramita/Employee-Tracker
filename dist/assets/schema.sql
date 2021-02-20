061e0890651bda8e08e727b474d0ced8dbd72e60
Amita <panwar.amita@gmail.com>
2021-02-19 21:16:48 -0500

    updated the schema.sql file

--- dist/assets/schema.sql
+++ dist/assets/schema.sql
@@ -1,4 +1,4 @@
-create an office database
+-- create an office database
 create database office_db;
  use office_db;
 -- create an department table
@@ -7,8 +7,7 @@ id int not null auto_increment,
 name varchar(30) not null,
 primary key(id)
 );
-create table role
- use office_db;
+-- create table role
 create table role(
 id int not null auto_increment,
 title varchar(30) not null,
@@ -17,9 +16,7 @@ department_id int,
 primary key(id),
 foreign key(department_id) references department(id)  
 );
-alter table role
-modify column salary decimal(10,5);
-create table employee
+-- create table employee
 create table employee(
 id int not null auto_increment,
 first_name varchar(30) not null,
@@ -37,40 +34,6 @@ insert into department(name)values('productions');
 -- insert records into role table
 insert into role(title,salary,department_id)values('Sales Lead',10000.10,1);
 insert into role(title,salary,department_id)values('Production Lead',9000.10,2);
-insert into employee table
+-- insert records into employee table
 insert into employee(first_name,last_name,role_id)values('Jhon','Doe',3);
 insert into employee(first_name,last_name,role_id,manager_id)values('Mike','Chan',3,1);
-query to select all the employee
-select e.id as id,e.first_name,e.last_name,r.title,d.name as department,r.salary,m.first_name as Manager_firstname,m.last_name as Manager_lastName
-from employee e inner join role r on e.role_id=r.id 
-inner join department d on r.department_id=d.id
-left join employee m on e.manager_id=m.id
-view employee by depatment
-select e.id as id,e.first_name,e.last_name,d.name as department
-from employee e right join role r on e.role_id=r.id 
-inner join department d on r.department_id=d.id
-order by e.id asc
-view all employee by manager
-query to select all the employee
-select e.id as id,e.first_name,e.last_name,m.first_name as Manager_firstname,m.last_name as Manager_lastName
-from employee e inner join employee m on m.manager_id=e.id
-
--- SELECT e1.id  as EmployeeId, e1.first_name as EmployeeName,  
---        e1.manager_id as  ManagerId, e2.first_name AS ManagerName 
--- FROM   employee e1 
---        LEFT JOIN employee e2 
---        ON e1.manager_id = e2.id
-use office_db;
-select * from employee;
-select * from role;
-update e.employee
-set e.role_id=2
-where employee;
--- select e.id as id,e.first_name,e.last_name,r.title,d.name as department,r.salary,m.first_name as Manager_firstname,m.last_name as Manager_lastName
---     from employee e inner join role r on e.role_id=r.id 
---     inner join department d on r.department_id=d.id
---     left join employee m on e.manager_id=m.id
---     order by e.id asc
--- SET FOREIGN_KEY_CHECKS = 0;
--- delete from employee where id=3; 0,
-  sql: 'SET FOREIGN_KEY_CHECKS = 0  delete from employee where id=7

4364a2b157e4d8ff178a49c960df911236b5cb88
Amita <panwar.amita@gmail.com>
2021-02-21 00:08:09 -0500

    updated the employee file

--- dist/assets/schema.sql
+++ dist/assets/schema.sql
@@ -1,5 +1,5 @@
 -- create an office database
-create database office_db;
+ -- create database office_db;
  use office_db;
 -- create an department table
 create table department(
@@ -33,9 +33,10 @@ insert into department(name)values('Engineering');
 insert into department(name)values('Finance');
 
 -- insert records into role table
-insert into role(title,salary,department_id)values('Sales Lead',1000000,1);
-insert into role(title,salary,department_id)values('Lawyer',800000,2);
+insert into role(title,salary,department_id)values('Sales Lead',10000,1);
+insert into role(title,salary,department_id)values('Lawyer',80000,2);
 insert into role(title,salary,department_id)values('SalesPerson',90000,2);
 -- insert records into employee table
-insert into employee(first_name,last_name,role_id,manager_id)values('Jhon','Doe',1);
-insert into employee(first_name,last_name,role_id,manager_id)values('Mike','Chan',3,1);
+insert into employee(first_name,last_name,role_id,manager_id)values('Jhon','Doe',3,2);
+insert into employee(first_name,last_name,role_id,manager_id)values('Mike','Chan',1,1);
+insert into employee(first_name,last_name,role_id,manager_id)values('Kevin','Tupik',2,2);

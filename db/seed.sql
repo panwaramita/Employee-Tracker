4174ac7c59e7ed7a270e45f57a492837259a465c
Amita <panwar.amita@gmail.com>
2021-02-23 22:59:02 -0500

    updated the db folder

--- db/seed.sql
+++ db/seed.sql
@@ -1,34 +1,4 @@
--- drop the database if already exist
-drop database if exists office_db;
--- create an office database
- create database office_db;
  use office_db;
--- create an department table
-create table department(
-id int not null auto_increment,
-name varchar(30) not null,
-primary key(id)
-);
--- create table role
-create table role(
-id int not null auto_increment,
-title varchar(30) not null,
-salary decimal(10,5) not null,
-department_id int,
-primary key(id),
-foreign key(department_id) references department(id)  
-);
--- create table employee
-create table employee(
-id int not null auto_increment,
-first_name varchar(30) not null,
-last_name varchar(30) not null,
-role_id int,
-manager_id int,
-primary key(id),
-foreign key(role_id) references role(id),
-foreign key(manager_id) references employee(id) 
-);
 -- insert records into department table
 insert into department(name)values('Sales');
 insert into department(name)values('Engineering');
